e9449c63874f12cc524552a6c33b8755234aa0c4
Murat Turan <murat47439@gmail.com>
2025-10-29 19:39:01 +0300

    fix

--- FrontEnd/src/lib/api/products/useGetProduct.ts
+++ FrontEnd/src/lib/api/products/useGetProduct.ts
@@ -5,6 +5,7 @@ import { IdParam, ProductDetail} from "@/lib/types/types";
 export async function getProduct(data: IdParam): Promise<ProductDetail> {
     try{
         const res = await api.get<ProductDetail>(`/products/${data.id}`)
+        console.log(res)
         return res.data
     }catch(err: unknown){
         const error = err as AxiosError<{ message: string }>;
